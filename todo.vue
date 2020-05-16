f8823050231f41fb455e1095faa898769bbfeefe
wendy03 <wendysu03@gmail.com>
2020-05-16 21:50:34 +0800

    todo vue

---
+++ todo.vue
@@ -0,0 +1,192 @@
+<template>
+  <div class="container my-3">
+    <h1 class="mb-5">Todo List</h1>
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon1">待辦事項</span>
+      </div>
+      <input
+        type="text"
+        class="form-control"
+        placeholder="準備要做的任務"
+        v-model="newTodo"
+        @keyup.enter="addTodo"
+      />
+      <div class="input-group-append">
+        <button class="btn btn-primary" type="button" @click="addTodo">
+          新增
+        </button>
+      </div>
+    </div>
+    <div class="card text-center">
+      <div class="card-header">
+        <ul class="nav nav-tabs card-header-tabs">
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              :class="{ active: visibility == 'all' }"
+              @click="visibility = 'all'"
+              href="#"
+              >全部</a
+            >
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              :class="{ active: visibility == 'active' }"
+              @click="visibility = 'active'"
+              href="#"
+              >進行中</a
+            >
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              :class="{ active: visibility == 'completed' }"
+              @click="visibility = 'completed'"
+              href="#"
+              >已完成</a
+            >
+          </li>
+        </ul>
+      </div>
+      <ul class="list-group list-group-flush text-left">
+        <li
+          class="list-group-item"
+          v-for="item in filteredTodos"
+          :key="item.id"
+          @dblclick="editTodo(item)"
+        >
+          <div class="d-flex" v-if="item.id !== cacheTodo.id">
+            <div class="form-check">
+              <input
+                type="checkbox"
+                class="form-check-input"
+                v-model="item.completed"
+                :id="item.id"
+              />
+              <label
+                class="form-check-label"
+                :class="{ completed: item.completed }"
+                :for="item.id"
+              >
+                {{ item.title }}
+              </label>
+            </div>
+            <button
+              type="button"
+              class="close ml-auto"
+              @click="removeTodo(item)"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <input
+            type="text"
+            class="form-control"
+            v-model="cacheTitle"
+            @keyup.esc="cancelEdit()"
+            @keyup.enter="doneEdit(item)"
+            v-if="item.id === cacheTodo.id"
+          />
+        </li>
+      </ul>
+      <div class="card-footer d-flex justify-content-between">
+        <span>還有 {{ countTodo }} 筆任務未完成</span>
+        <a href="#" @click="removeAll">清除所有任務</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodo: '',
+      todos: [],
+      cacheTodo: {},
+      cacheTitle: '',
+      visibility: 'all',
+    };
+  },
+  methods: {
+    addTodo: function () {
+      const value = this.newTodo.trim();
+      const timestamp = Math.floor(Date.now());
+      if (!value) {
+        return;
+      }
+      this.todos.push({
+        id: timestamp,
+        title: value,
+        completed: false,
+      });
+      this.newTodo = '';
+    },
+    removeTodo: function (todo) {
+      const vm = this;
+      const newIndex = vm.todos.findIndex(function (item) {
+        return todo.id === item.id;
+      });
+      this.todos.splice(newIndex, 1);
+    },
+    removeAll: function () {
+      this.todos = [];
+    },
+    editTodo: function (item) {
+      this.cacheTodo = item;
+      this.cacheTitle = item.title;
+    },
+    cancelEdit: function () {
+      this.cacheTodo = {};
+    },
+    doneEdit: function (item) {
+      item.title = this.cacheTitle;
+      this.cacheTodo = {};
+      this.cacheTitle = '';
+    },
+  },
+  computed: {
+    filteredTodos: function () {
+      if (this.visibility == 'all') {
+        return this.todos;
+      } else if (this.visibility == 'active') {
+        const newTodos = [];
+        this.todos.forEach(function (item) {
+          if (!item.completed) {
+            newTodos.push(item);
+          }
+        });
+        return newTodos;
+      } else if (this.visibility == 'completed') {
+        const newTodos = [];
+        this.todos.forEach(function (item) {
+          if (item.completed) {
+            newTodos.push(item);
+          }
+        });
+        return newTodos;
+      }
+      return [];
+    },
+    countTodo: function () {
+      let count = 0;
+      this.todos.forEach(function (todo) {
+        if (!todo.completed) {
+          count += 1;
+        }
+      });
+      return count;
+    },
+  },
+};
+</script>
+
+<style>
+@import url(css/all.css);
+.completed {
+  text-decoration: line-through;
+}
+</style>
